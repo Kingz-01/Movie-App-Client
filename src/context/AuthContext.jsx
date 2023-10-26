be408ca9e10d65b122929c8064bf6fdbce3f9700
kingz-01 <kingsleyoffor@yahoo.com>
2023-10-26 14:38:29 +0100

    first commit

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,111 @@
+import { createContext, useState } from "react";
+// import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../utils/axiosConfig";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+// context provider
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(
+    () => JSON.parse(localStorage.getItem("token")) || null
+  );
+
+  const [authenticating, setAuthenticating] = useState(false);
+
+  const navigate = useNavigate();
+
+  // register
+  const handleRegisterUser = async (formData) => {
+    // do logic for register
+    try {
+      const { data } = await axiosInstance.post("/api/auth/register", formData);
+
+      toast.success("Registration Successful");
+      localStorage.setItem("token", JSON.stringify(data.token));
+      setToken(data.token);
+      setUser({ id: data.id });
+      navigate("/");
+    } catch (error) {
+      if (error.response) {
+        
+        return toast.error(error.response.data.message);
+      }
+
+      toast.error("Something went wrong");
+    }
+  };
+
+  // Sign in
+  const handleSignInUser = async (formData) => {
+    setAuthenticating(true);
+    // do logic for sign in
+    setTimeout(async () => {
+      try {
+        const { data } = await axiosInstance.post("/api/auth/login", formData);
+
+        toast.success("Welcome Back");
+        localStorage.setItem("token", JSON.stringify(data.token));
+        setToken(data.token);
+        setUser({ id: data.id });
+        navigate("/");
+      } catch (error) {
+        if (error.response) {
+          
+          return toast.error(error.response.data.message);
+        }
+
+        toast.error("Something went wrong");
+      } finally {
+        setAuthenticating(false);
+      }
+    }, 2000);
+  };
+
+  const handleGetUser = async () => {
+    try {
+      const { data } = await axiosInstance.post(
+        "/api/auth/user",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+     
+      setUser(data);
+    } catch (error) {
+      
+      if (error.message === "Network Error") {
+      }
+    }
+  };
+
+  const handleLogOutUser = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("token");
+    toast.success("See you soon");
+    navigate("/");
+  };
+
+  const contextData = {
+    user,
+    token,
+    handleRegisterUser,
+    handleSignInUser,
+    authenticating,
+    handleGetUser,
+    handleLogOutUser,
+  };
+
+  return (
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  );
+};
